7785337710776646dfd9c5e64f4d835ef6650e8b
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-17 01:42:13 -0700

    Tweethandler implementation with form

--- tweetsourcing/main/routes.py
+++ tweetsourcing/main/routes.py
@@ -1,10 +1,32 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, flash
+from tweetsourcing.search import tweethandler
+from tweetsourcing.main.forms import TweetForm
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 
 @bp.route("", methods=("GET", "POST"))
 def home():
+    form = TweetForm()
     if request.method == "POST":
-        tweet_url = request.form["tweet_url"]
-    return render_template("home.html", title="TweetSourcing")
+        tweet_url = form.tweet_url.data
+        tweet_status = tweethandler.retrieve_tweet(
+            tweethandler.create_api(), tweet_url=tweet_url
+        )
+        tweet_images = tweethandler.pull_images(tweet_status)
+        return render_template(
+            "home.html",
+            title="TweetSourcing",
+            tweet_status=tweet_status.full_text,
+            tweet_images=tweet_images,
+            form=form,
+        )
+
+    else:
+        return render_template(
+            "home.html",
+            title="TweetSourcing",
+            tweet_status=None,
+            tweet_images=None,
+            form=form,
+        )
