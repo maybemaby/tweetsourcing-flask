5ad05131b74e06b3f5e718ca3cac17b5b6dd04d5
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-17 00:31:30 -0700

    Template setup

--- tweetsourcing/main/routes.py
+++ tweetsourcing/main/routes.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, request, url_for
 
-bp = Blueprint('main', __name__, url_prefix='/')
+bp = Blueprint("main", __name__, url_prefix="/")
 
-@bp.route("")
-def test():
-    return "Hello, World!"
+
+@bp.route("", methods=("GET", "POST"))
+def home():
+    if request.method == "POST":
+        tweet_url = request.form["tweet_url"]
+    return render_template("home.html", title="TweetSourcing")
