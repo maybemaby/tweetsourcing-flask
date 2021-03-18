f34b5051364ea0131f5643989ebb77c1402bd49b
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-17 21:46:06 -0700

    RAKE functions written

---
+++ tweetsourcing/search/parse.py
@@ -0,0 +1,20 @@
+from rake_nltk import Rake
+
+
+def extract_kwords(tweet_object: tweepy.Status) -> list:
+    """Uses RAKE algorithm to extract keywords from a tweet.
+
+    :param tweet_object: Tweepy Status object generated from url
+    :type tweet_object: tweepy.Status
+    :return: [description]
+    :rtype: [type]
+    """
+    r = Rake()
+    tweet_text = tweet_object.full_text
+    r.extract_keywords_from_text(tweet_text)
+    return r.get_ranked_phrases()
+
+def create_query(kword_list):
+    """Takes keyword list and joins them to be used in google search."""
+    query = " OR ".join(kword_list)
+    return query
