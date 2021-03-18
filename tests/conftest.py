1d4cd06df55a886ab55eb95937fcdb312c15de85
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-17 19:30:55 -0700

    Tests init

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import os
+import pytest
+from tweetsourcing import create_app
+
+@pytest.fixture
+def app():
+    app = create_app({
+        'TESTING': True,
+    })
+
+    yield app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def runner(app):
+    return app.test_cli_runner()
