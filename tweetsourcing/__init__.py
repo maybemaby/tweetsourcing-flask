87b056c047118e6cf2dcf756829a919fbb285711
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-26 22:39:29 -0700

    logger setup

--- tweetsourcing/__init__.py
+++ tweetsourcing/__init__.py
@@ -1,5 +1,6 @@
+import logging
 import os
-
+from logging.handlers import RotatingFileHandler
 from flask import Flask, current_app
 from tweetsourcing.search.tweethandler import TweetHandler
 from config import Config
@@ -21,9 +22,20 @@ def create_app(config_class=Config):
     from tweetsourcing.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/tweetsourcing.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(
+            logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        )
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('App startup')
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
+    
     return app
