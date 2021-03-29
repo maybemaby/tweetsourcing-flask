3d65646acf9a9359da802f65bfb11fbbed13fece
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-28 22:15:30 -0700

    Error pages look nice

--- tweetsourcing/errors/handlers.py
+++ tweetsourcing/errors/handlers.py
@@ -1,14 +1,16 @@
-from flask import render_template
+from flask import render_template, request, current_app
 from tweepy.models import ResultSet
 from tweetsourcing.errors import bp
 
 @bp.app_errorhandler(404)
 def not_found_error(error):
+    current_app.logger.error(f'404 Error to {request.url} with {request.method}')
     return render_template('errors/404.html'), 404
 
 
 @bp.app_errorhandler(500)
 def internal_error(error):
+    current_app.logger.error(f'500 Error to {request.url} with {request.method}')
     return render_template('errors/500.html'), 500
 
 @bp.route("no-results")
