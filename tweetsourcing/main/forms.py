49f1552d5f74e53f2758c97e231418bc28b475b2
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-03-28 23:58:05 -0700

    Added twitter specific URL validation

--- tweetsourcing/main/forms.py
+++ tweetsourcing/main/forms.py
@@ -1,19 +1,37 @@
+import re
 from flask.app import Flask
 from flask_wtf import FlaskForm
 from wtforms.fields.html5 import URLField
 from wtforms.fields import SubmitField, StringField, BooleanField
-from wtforms.validators import DataRequired, Length, URL
+from wtforms.validators import DataRequired, Length, URL, ValidationError
+
+
+def twitter_url_check(form, field):
+    """Custom validator for ensuring URL is twitter link."""
+    tweet_regex = re.compile(r"(.+/status/\d+)(\?s=\d+)?", flags=re.IGNORECASE)
+    tweet_mo = re.search(tweet_regex, field.data)
+    if type(tweet_mo) is not re.Match:
+        raise ValidationError("Must be a Tweet Status URL")
 
 
 class TweetForm(FlaskForm):
     tweet_url = URLField(
         "Enter a tweet URL",
-        validators=[DataRequired(message="Please fill in a valid URL"), URL(message="Please enter in a valid URL")],
+        validators=[
+            DataRequired(message="Please fill in a valid URL"),
+            URL(message="Please enter in a valid URL"),
+            twitter_url_check,
+        ],
     )
-    submit = SubmitField('Find Sources')
+    submit = SubmitField("Find Sources")
+
 
 class SearchForm(FlaskForm):
     query = StringField(validators=[Length(max=141, message="Query too long")])
-    or_terms = StringField(validators=[Length(max=141, message="Additional terms too long")])
-    img_search = BooleanField(label="Reverse Image Search", default=False, id="img_search")
-    submit = SubmitField('Confirm')
+    or_terms = StringField(
+        validators=[Length(max=141, message="Additional terms too long")]
+    )
+    img_search = BooleanField(
+        label="Reverse Image Search", default=False, id="img_search"
+    )
+    submit = SubmitField("Confirm")
