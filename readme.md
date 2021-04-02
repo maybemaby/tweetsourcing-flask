266913628e1676103694f781cb3ccad2e09b7452
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-04-01 21:06:05 -0700

    Added packaging files

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # TweetSourcing Flask App
 
-Modules used: google-cloud-vision, flask, flask-wtf, google-api-python-client, newspaper3k, pytest, rake-nltk
+Modules used: google-cloud-vision, flask, flask-wtf, google-api-python-client, newspaper3k, pytest, rake-nltk, dotenv
 
 ## Usage
 ```
@@ -11,8 +11,10 @@ $ tweetsourcing\venv\Scripts\activate.bat
 
 ```
 git clone https://github.com/maybemaby/tweetsourcing-flask.git
-pip install requirements.txt
+pip install -e
 ```
+
+Fill in secure secret key and valid api credentials to the generated .env file.
 
 # Features
 - RAKE keyword extraction from tweets
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+if not os.path.exists('.env'):
+    with open(".env", "w", encoding="utf-8") as envfile:
+        envfile.write('''SECRET_KEY=not-so-secret
+        FLASK_APP=tweetsourcing
+        FLASK_ENV=development
+        TWITTER_API_KEY=
+        TWITTER_SECRET_KEY=
+        TWITTER_ACCESS_TOKEN=
+        TWITTER_ACCESS_TOKEN_SECRET=
+        CSE_API_KEY=
+        CSE_ID=
+        GOOGLE_APPLICATION_CREDENTIALS=''')
+        envfile.close()
+
+
+setup(
+    name='tweetsourcing',
+    version='1.0.0',
+    author='Brandon Ma',
+    description='Flask web app for finding secondary sources of tweet info',
+    url='https://github.com/maybemaby/tweetsourcing-flask',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask-wtf',
+        'google-cloud-vision',
+        'google-api-python-client',
+        'newspaper3k',
+        'python-dotenv',
+        'rake-nltk',
+    ],
+)
