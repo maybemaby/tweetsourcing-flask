ff84a90dad35236ba8d950a09479aacc8466d82d
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-04-01 23:24:01 -0700

    Adding stdout logging functionality for heroku

--- tweetsourcing/__init__.py
+++ tweetsourcing/__init__.py
@@ -23,16 +23,21 @@ def create_app(config_class=Config):
     app.register_blueprint(errors_bp)
 
     if not app.debug and not app.testing:
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/tweetsourcing.log', maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(
-            logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-        )
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('App startup')
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/tweetsourcing.log', maxBytes=10240, backupCount=10)
+            file_handler.setFormatter(
+                logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+            )
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('App startup')
     try:
         os.makedirs(app.instance_path)
     except OSError:
